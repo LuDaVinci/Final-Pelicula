f526d6c78a820f6f89ab02c7641d9425cf54cd87
LuDaVinci <lucia.piazzi@davinci.edu.ar>
2025-08-06 19:12:50 -0300

    mh.2

--- java/script.js
+++ java/script.js
@@ -1,9 +1,28 @@
 
+
 document.addEventListener("DOMContentLoaded", function () {
   const toggle = document.getElementById("menu-toggle");
-  const menu = document.querySelector("nav .menu");
+  const menu = document.getElementById("main-menu");
 
   toggle.addEventListener("click", () => {
-    menu.classList.toggle("show");
+    if (menu.style.display === "flex" || menu.style.display === "") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "flex";
+    }
   });
 });
+
+let currentIndex = 0;
+
+function moveCarousel(direction) {
+  const carousel = document.getElementById('carousel');
+  const totalCards = carousel.children.length;
+  currentIndex += direction;
+
+  if (currentIndex < 0) currentIndex = totalCards - 1;
+  if (currentIndex >= totalCards) currentIndex = 0;
+
+  const offset = -currentIndex * 100;
+  carousel.style.transform = `translateX(${offset}%)`;
+}
