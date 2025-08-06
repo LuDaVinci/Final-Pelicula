621a21d866be14eaaef4a06f07758205c6c14295
LuDaVinci <lucia.piazzi@davinci.edu.ar>
2025-08-06 20:49:14 -0300

    mh.8

--- java/script.js
+++ java/script.js
@@ -1,24 +1,34 @@
 
 
 document.addEventListener("DOMContentLoaded", function () {
-  const toggle = document.getElementById("menu-toggle");
-  const menu = document.querySelector("nav ul.menu");
+    const toggle = document.getElementById("menu-toggle");
+    const menu = document.querySelector("nav ul.menu");
 
-  toggle.addEventListener("click", function () {
-    menu.classList.toggle("show");
-  });
+    toggle.addEventListener("click", function () {
+        menu.classList.toggle("show");
+    });
+
+    // Submenú individual
+    const submenuToggles = document.querySelectorAll(".submenu-toggle");
+
+    submenuToggles.forEach(function (arrow) {
+        arrow.addEventListener("click", function () {
+            const submenu = this.nextElementSibling;
+            submenu.classList.toggle("open");
+        });
+    });
 });
 
 let currentIndex = 0;
 
 function moveCarousel(direction) {
-  const carousel = document.getElementById('carousel');
-  const totalCards = carousel.children.length;
-  currentIndex += direction;
+    const carousel = document.getElementById('carousel');
+    const totalCards = carousel.children.length;
+    currentIndex += direction;
 
-  if (currentIndex < 0) currentIndex = totalCards - 1;
-  if (currentIndex >= totalCards) currentIndex = 0;
+    if (currentIndex < 0) currentIndex = totalCards - 1;
+    if (currentIndex >= totalCards) currentIndex = 0;
 
-  const offset = -currentIndex * 100;
-  carousel.style.transform = `translateX(${offset}%)`;
+    const offset = -currentIndex * 100;
+    carousel.style.transform = `translateX(${offset}%)`;
 }
