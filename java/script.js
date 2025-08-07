11e9035e6aceb458b3ccbe5380556cbd5f7f2504
LuDaVinci <lucia.piazzi@davinci.edu.ar>
2025-08-06 21:04:55 -0300

    n c

--- java/script.js
+++ java/script.js
@@ -42,16 +42,3 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-let currentIndex = 0;
-
-function moveCarousel(direction) {
-    const carousel = document.getElementById('carousel');
-    const totalCards = carousel.children.length;
-    currentIndex += direction;
-
-    if (currentIndex < 0) currentIndex = totalCards - 1;
-    if (currentIndex >= totalCards) currentIndex = 0;
-
-    const offset = -currentIndex * 100;
-    carousel.style.transform = `translateX(${offset}%)`;
-}
