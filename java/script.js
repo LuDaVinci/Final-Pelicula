32e2786d38b64de383cbdedfaa8a324b14e503a1
LuDaVinci <lucia.piazzi@davinci.edu.ar>
2025-08-07 00:07:14 -0300

    mh.20

--- java/script.js
+++ java/script.js
@@ -20,14 +20,14 @@ submenuItems.forEach(item => {
         i.classList.remove('open');
         i.querySelector('.submenu').style.display = 'none';
         i.querySelector('.arrow-icon svg').innerHTML =
-          '<path fill=\"#FFFFFF\" d=\"M480-344 240-584l43-43 197 197 197-197 43 43-240 240Z\"/>';
+          '<path fill="#FFFFFF" d="M480-344 240-584l43-43 197 197 197-197 43 43-240 240Z"/>';
       });
 
       if (!isOpen) {
         item.classList.add('open');
         item.querySelector('.submenu').style.display = 'block';
         item.querySelector('.arrow-icon svg').innerHTML =
-          '<path fill=\"#f8d300\" d=\"M480-554 283-357l-43-43 240-240 240 240-43 43-197-197Z\"/>';
+          '<path fill="#f8d300" d="M480-554 283-357l-43-43 240-240 240 240-43 43-197-197Z"/>';
       }
     }
   });
